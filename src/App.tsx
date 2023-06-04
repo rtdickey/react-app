025e7e2d5eed21f4ee455d5b8acdc12c2ed5de9b
Ryan Dickey <ryantay87@gmail.com>
2023-06-04 13:38:34 -0500

    Adding Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import ListGroup from "./components/ListGroup";
-import "./App.css";
+import { BsFillCalendarFill } from "react-icons/bs";
 
 function App() {
   let items = ["New York", "Los Angeles", "San Francisco"];
@@ -9,11 +8,7 @@ function App() {
 
   return (
     <div>
-      <ListGroup
-        heading="Miami"
-        items={items}
-        onSelectItem={handleSelectItem}
-      />
+      <BsFillCalendarFill color="red" size="40" />
     </div>
   );
 }
