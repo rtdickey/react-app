b183a5170db54b8fc1388353e31e8adf9c662e56
Ryan Dickey <ryantay87@gmail.com>
2023-06-04 08:52:59 -0500

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -9,11 +9,9 @@ function App() {
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
