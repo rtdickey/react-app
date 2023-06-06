b761c308f32bcb56facc88c854ccc489a77b9432
Ryan Dickey <ryantay87@gmail.com>
2023-06-05 19:31:06 -0500

    Choosing the state structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Like from "./components/Like";
 
 function App() {
-  return (
-    <div>
-      <Like onClick={() => console.log("clicked")} />
-    </div>
-  );
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  const [isLoading, setLoading] = useState(false);
+
+  return <div></div>;
 }
 
 export default App;
