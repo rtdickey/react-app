c127cbc0ed5d22c71bb0ecdd38e152d30d0919a7
Ryan Dickey <ryantay87@gmail.com>
2023-06-05 19:57:29 -0500

    Managing State: Simplify Update Logic with Immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Like from "./components/Like";
+import produce from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
@@ -8,11 +8,22 @@ function App() {
   ]);
 
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    //setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   return (
     <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
