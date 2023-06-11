19c39471579e7ca9fe33c5a7a70cc71ca0459f4a
Ryan Dickey <ryantay87@gmail.com>
2023-06-11 13:10:43 -0500

    Applying Validation.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,17 @@
 import { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
-  console.log(register("name"));
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -14,22 +22,34 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be at least 3 characters.</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { required: true, min: 1 })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age?.type === "required" && (
+          <p className="text-danger">The age is required.</p>
+        )}
+        {errors.age?.type === "min" && (
+          <p className="text-danger">The age must be greater than 0</p>
+        )}
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
