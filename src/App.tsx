e4f96606f375b3c5c40bb62618a5a1d1b235e508
Ryan Dickey <ryantay87@gmail.com>
2023-06-29 17:48:14 -0500

    Effect Clean Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 import ProductList from "./components/ProductList";
 
-function App() {
-  const [category, setCategory] = useState("");
+const connect = () => console.log("Connecting");
 
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  );
+const disconnect = () => console.log("Disconnecting");
+
+function App() {
+  useEffect(() => {
+    connect();
+
+    //Clean up function
+    return () => disconnect();
+  });
+  return <div></div>;
 }
 
 export default App;
