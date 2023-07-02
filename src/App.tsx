530ef1dbb6297b5f10a950d1fc8f336bfe207519
Ryan Dickey <ryantay87@gmail.com>
2023-07-01 22:20:43 -0500

    Creating a custom data fetching hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import { useState, useEffect } from "react";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const addUser = () => {
     const originalUsers = [...users];
